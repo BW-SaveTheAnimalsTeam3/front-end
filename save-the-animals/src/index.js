0008716083e808cdc665e407102ace2bcc2c0561
Logan Sorensen <loganmsorensen@gmail.com>
2020-02-01 16:53:54 -0800

    set up provider

--- save-the-animals/src/index.js
+++ save-the-animals/src/index.js
@@ -1,6 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+
+import App from "./App";
+import rootReducer from "./reducers";
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
