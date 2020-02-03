455e8e4910b83ae937eb7c80178bb3ef9e7d304d
johnkirtley <kirtleyj16@gmail.com>
2020-02-03 10:29:50 -0800

    Created initial tabs for login

--- save-the-animals/src/App.js
+++ save-the-animals/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Login from './components/Organization/Login';
 import "./App.css";
 
 function App() {
-  return <div className="App">
-  </div>;
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  )
 }
 
 export default App;
