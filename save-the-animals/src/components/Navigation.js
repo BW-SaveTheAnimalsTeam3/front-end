e81db32a0514e44835a84dda2f4d6bcdef1e4dd5
johnkirtley <kirtleyj16@gmail.com>
2020-02-04 11:07:38 -0800

    Added 'Home' to navigation that directs to marketing page

--- save-the-animals/src/components/Navigation.js
+++ save-the-animals/src/components/Navigation.js
@@ -9,6 +9,7 @@ export default function Navigation() {
     return (
         <div>
             <ul className="nav-list">
+                <a href="https://savetheanimalsweb28.netlify.com/" className="menu-item">Home</a>
                 <Link to='/organization' className='menu-item'>Dashboard</Link>
                 <Link to='organization' className='menu-item'>Profile</Link>
                 <Link to='/' className='menu-item'>Log Out</Link>
