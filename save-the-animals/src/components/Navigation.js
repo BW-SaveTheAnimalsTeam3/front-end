25fecf4daef106b867a660ab833e5282e9ba9bef
johnkirtley <kirtleyj16@gmail.com>
2020-02-04 11:00:24 -0800

    Added navigation to Create Campaign component

--- save-the-animals/src/components/Navigation.js
+++ save-the-animals/src/components/Navigation.js
@@ -9,7 +9,7 @@ export default function Navigation() {
     return (
         <div>
             <ul className="nav-list">
-                <Link to='/' className='menu-item'>Dashboard</Link>
+                <Link to='/organization' className='menu-item'>Dashboard</Link>
                 <Link to='organization' className='menu-item'>Profile</Link>
                 <Link to='/' className='menu-item'>Log Out</Link>
             </ul>
