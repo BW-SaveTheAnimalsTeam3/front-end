7bbce69805727e31a40af990eb675f98a19a58f5
johnkirtley <kirtleyj16@gmail.com>
2020-02-04 10:19:08 -0800

    Added nav bar to organization page

---
+++ save-the-animals/src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+
+
+
+export default function Navigation() {
+
+
+    return (
+        <div>
+            <ul className="nav-list">
+                <li>Dashboard</li>
+                <li>Profile</li>
+                <li>Log Out</li>
+            </ul>
+        </div>
+    )
+}
--- save-the-animals/src/components/profile/organization-profile.jsx
+++ save-the-animals/src/components/profile/organization-profile.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import CampaignCard from '../card/campaign-card';
+import Navigation from '../Navigation';
 
 const OrganizationProfile = (props) => {
 
@@ -10,23 +11,26 @@ const OrganizationProfile = (props) => {
   }
 
   return (
-    <div className='profile'>
-      <div className='user-info'>
-        <div className='profile-image'></div>
-        <div className='user-info-text'>
-        <h2>Caribbean Sea Turtle Project</h2>
-        <p>St. George's, Grenada</p>
-        <p>Conservation Organization</p>
+    <>
+      <Navigation />
+      <div className='profile'>
+        <div className='user-info'>
+          <div className='profile-image'></div>
+          <div className='user-info-text'>
+            <h2>Caribbean Sea Turtle Project</h2>
+            <p>St. George's, Grenada</p>
+            <p>Conservation Organization</p>
+          </div>
+        </div>
+        <div className='feed-buttons'>
+          <h3><span>Campaigns</span></h3>
+          <h3 onClick={handleClick}>Start New Campaign</h3>
+        </div>
+        <div className='feed'>
+          <CampaignCard />
         </div>
       </div>
-      <div className='feed-buttons'>
-        <h3><span>Campaigns</span></h3>
-        <h3 onClick={handleClick}>Start New Campaign</h3>
-      </div>
-      <div className='feed'>
-        <CampaignCard />
-      </div>
-    </div>
+    </>
   );
 };
 
